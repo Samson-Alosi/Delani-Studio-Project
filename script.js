630cc009990712e2a497a7e1c5b6a0a05e4f39d8
Samson-Alosi <psalmchrisam@gmail.com>
2021-07-24 18:35:06 +0300

    added JS trial toggle

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".product-management").on("click",function(){
+        $(".management1").toggle();
+        $(".product-managements").toggle(); 
+    });
+    
+    $(".design").on("click",function(){
+        $(".desi1").toggle();
+        $(".design11").toggle();
+        
+        
+    });
+
+    $(".development").on("click",function(){
+        $(".development1").toggle();
+        $(".deviy").toggle();
+    });
+
+
+});
