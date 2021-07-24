8dd02867b3f21de99c17a81bc4f652591f6a0604
Samson-Alosi <psalmchrisam@gmail.com>
2021-07-25 01:58:07 +0300

    added hooverJS

--- script.js
+++ script.js
@@ -38,3 +38,97 @@ $(document).ready(function () {
                $(".image1").css("opacity","3");
     });
 }); 
+
+  $(document).ready(function() {
+    $(".mighty1").mouseenter(function() {
+               $(".mighty").show();
+               $(".mighty1").css("opacity","0.7");
+    });
+    $(".mighty1").mouseleave(function() {
+               $(".mighty").hide();
+               $(".mighty1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".yellow1").mouseenter(function() {
+               $(".yellow").show();
+               $(".yellow1").css("opacity","0.7");
+    });
+    $(".yellow1").mouseleave(function() {
+               $(".yellow").hide();
+               $(".yellow1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".portfo1").mouseenter(function() {
+               $(".portfo").show();
+               $(".portfo1").css("opacity","0.7");
+    });
+    $(".portfo1").mouseleave(function() {
+               $(".portfo").hide();
+               $(".portfo1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".landing1").mouseenter(function() {
+               $(".landing").show();
+               $(".landing1").css("opacity","0.7");
+    });
+    $(".landing1").mouseleave(function() {
+               $(".landing").hide();
+               $(".landing1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".image1").mouseenter(function() {
+               $(".image").show();
+               $(".image1").css("opacity","0.7");
+    });
+    $(".image1").mouseleave(function() {
+               $(".image").hide();
+               $(".image1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".button1").mouseenter(function() {
+               $(".button").show();
+               $(".button1").css("opacity","0.7");
+    });
+    $(".button1").mouseleave(function() {
+               $(".button").hide();
+               $(".button1").css("opacity","3");
+    });
+});
+
+  $(document).ready(function() {
+    $(".burned1").mouseenter(function() {
+               $(".burned").show();
+               $(".burned1").css("opacity","0.7");
+    });
+    $(".burned1").mouseleave(function() {
+               $(".burned").hide();
+               $(".burned1").css("opacity","3");
+    });
+}); 
+
+  $(document).ready(function() {
+    $(".giraffe1").mouseenter(function() {
+               $(".giraffe").show();
+               $(".giraffe1").css("opacity","0.7");
+    });
+    $(".giraffe1").mouseleave(function() {
+               $(".giraffe").hide();
+               $(".giraffe1").css("opacity","3");
+    });
+}); 
+
+
+
+
+
+
